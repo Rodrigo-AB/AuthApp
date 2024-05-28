6a6f0b85a0f3e2bebff96e1ea8dc554283af8ea5
Rodrigo-AB <p_rodrigo_ab@hotmail.com>
2024-05-28 16:05:54 -0600

    terminada la seccion 26, terminamos la base de la aplicacion. a esto le hace falta mucho trabajo por delante, pero la base ya esta bastante solida y funcional

---
+++ src/app/auth/interfaces/auth-status.enum.ts
@@ -0,0 +1,6 @@
+
+export  enum  AuthStatus  { //Las enumeracions nos permiten tener el control sobre los numeros de posibles vlores
+  checking          = 'checking,',
+  authenticated     = 'authenticated,',
+  notAuthenticated  = 'notAuthenticated,',
+}
